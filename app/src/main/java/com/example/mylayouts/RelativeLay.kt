0f40029b620729e24da71700b7c738978115e5c0
pujakumari <singhpuja7549@gmail.com>
2022-02-02 17:44:26 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mylayouts/RelativeLay.kt
@@ -0,0 +1,11 @@
+package com.example.mylayouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLay : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_lay)
+    }
+}
